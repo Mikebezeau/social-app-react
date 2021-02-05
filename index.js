e5db7f25a5ce821b6d7e7f0bbb3d83bc3c2755b3
Mikebezeau <mikebezeau@gmail.com>
2021-02-05 15:05:10 -0500

    Update env variable for port

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const { MONGO_DB } = require('./config.js');
+const { MONGO_DB } = require("./config.js");
 
-const typeDefs = require('./graphql/TypeDefs');
-const resolvers = require('./graphql/resolvers')
+const typeDefs = require("./graphql/TypeDefs");
+const resolvers = require("./graphql/resolvers");
 
 const pubsub = new PubSub();
+
+const PORT = prcess.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req, pubsub })
+  context: ({ req }) => ({ req, pubsub }),
 });
 
-mongoose.connect(MONGO_DB, { useNewUrlParser: true })
+mongoose
+  .connect(MONGO_DB, { useNewUrlParser: true })
   .then(() => {
-    console.log('Mongo connected');
-    return server.listen({ port: 5000});
-  }).then(res => {
-      console.log(`Server running at ${res.url}`);
+    console.log("Mongo connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
-
-  
